6aefc035005367744227cc4485195be8e6b4aa8d
araujo-vk <victor.oliveira7782@gmail.com>
2026-09-13 19:08:07 -0300

    início do CSS

--- config/express.js
+++ config/express.js
@@ -1,12 +1,11 @@
 var express = require('express'); 
-
 var app = express();
-
+const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '../public')));
 module.exports = function () {
   return app;
 };
